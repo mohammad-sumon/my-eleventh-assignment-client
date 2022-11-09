1ff6acf0b7bd27c58c9dc522ed71264f35830ef7
Mohammad Sumon <sumon3011@gmail.com>
2022-11-09 11:39:02 +0600

    react photo view added

--- src/pages/AllServicesDetails/AllServicesDetails.js
+++ src/pages/AllServicesDetails/AllServicesDetails.js
@@ -4,13 +4,21 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
 
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
+
 
 const AllServicesDetails = ({ service }) => {
   
   return (
     <Container>
         <Card className="w-full shadow-xl">
-        <Card.Img className="w-96" variant="top" src={service.img} alt="" />
+
+        <PhotoProvider>
+        <PhotoView src={service.img}>
+          <Card.Img className="w-96" variant="top" src={service.img} alt="" />
+        </PhotoView>
+      </PhotoProvider>
         <Card.Body>
           <Card.Title>{service.name}</Card.Title>
           <Card.Text>BDT: {service.price}</Card.Text>
--- src/pages/Home/HomePageServices/HomePageServices.js
+++ src/pages/Home/HomePageServices/HomePageServices.js
@@ -2,11 +2,18 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
+
 const HomePageServices = ({ service }) => {
   return (
     <div>
       <Card className="w-full shadow-xl">
-        <Card.Img variant="top" src={service.img} alt=""/>
+      <PhotoProvider>
+        <PhotoView src={service.img}>
+          <Card.Img className="w-96" variant="top" src={service.img} alt="" />
+        </PhotoView>
+      </PhotoProvider>
         <Card.Body>
           <Card.Title>{service.name}</Card.Title>
           <Card.Text>BDT: {service.price}</Card.Text>
